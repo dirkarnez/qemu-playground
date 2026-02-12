cc44b6fdf25efd00c75ffa83d6d3efbef52e8229
Dirk Arnez <smalldirkalex@gmail.com>
2026-02-12 15:15:59 +0800

    - update qemus.sh

--- qemus.sh
+++ qemus.sh
@@ -2,5 +2,7 @@
 
 echo "User: $(whoami) UID: $(id -u) GID: $(id -g)"
 
-sudo apt install cpu-checker
+sudo apt install cpu-checker && \
 sudo kvm-ok
+
+docker compose up --build && docker compose down
