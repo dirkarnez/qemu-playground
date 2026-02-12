b6ea42bfde62aa1b0a061a95609d06b3b64ff450
Dirk Arnez <smalldirkalex@gmail.com>
2026-02-12 15:14:22 +0800

    - update qemus.sh

--- qemus.sh
+++ qemus.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo "User: $(whoami) UID: $(id -u) GID: $(id -g)"
-docker run -it --rm --name qemu -e "BOOT=mint" -p 8006:8006 --device=/dev/kvm --device=/dev/net/tun --cap-add NET_ADMIN -v "${PWD:-.}/qemu:/storage" --stop-timeout 120 docker.io/qemux/qemu
+
+sudo apt install cpu-checker
+sudo kvm-ok
